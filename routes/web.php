7efd6b694216ea12ade1744c593b3fdd0e2f37e3
Shweryl <shwezin2001shwezin2001@gmail.com>
2025-02-14 01:26:27 +0630

    editing index route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect(route('home'));
+    return view('welcome');
 });
 
 Auth::routes();
