05bb928793c9b338366970cae2934922e533ed9c
Michael <hnthu.aung@gmail.com>
2025-02-17 23:21:35 +0630

    chat room id creating method implemenation

--- app/Http/Controllers/Api/V1/ChatController.php
+++ app/Http/Controllers/Api/V1/ChatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AddChatRequest;
 use App\Http\Resources\ChatCollection;
 use App\Http\Resources\ChatResource;
 use App\Models\Chat;
@@ -21,7 +22,6 @@ class ChatController extends Controller
             $chat = Chat::where('user_id', $user->id)->get();
 
             return response()->success($request, new ChatCollection($chat), 'Retrieve chat list successful', 200);
-
         } catch (Exception $e) {
 
             Log::error($e->getMessage());
@@ -30,5 +30,31 @@ class ChatController extends Controller
         }
     }
 
+    public function store(AddChatRequest $request)
+    {
+        try {
+            $validated = $request->validated();
 
+            $user = $request->user();
+
+            $validated['user_id'] = $user->id;
+
+            $chat = Chat::where('user_id', $user->id)
+                ->where('mentor_id', $validated['mentor_id'])
+                ->first();
+
+
+            if (!$chat) {
+                $chat = Chat::create($validated);
+            }
+
+            return response()->success($request,['chat_id' => $chat->id], 'Creating chat room successful', 201);
+
+        } catch (Exception $e) {
+
+            Log::error($e->getMessage());
+
+            return response()->error($request, null, 'Internal Server Error', 500);
+        }
+    }
 }
