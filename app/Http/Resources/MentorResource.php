865dd94b4f6851197b2ae3643c5d0c31f7c7ad2e
Michael <hnthu.aung@gmail.com>
2025-02-19 18:03:17 +0630

    mentro id return fix

--- app/Http/Resources/MentorResource.php
+++ app/Http/Resources/MentorResource.php
@@ -18,7 +18,7 @@ class MentorResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            'id' => $this->id,
+            'mentor_id' => $this->mentor->id,
             'image' => $this->profile_picture ? asset("storage/profile_pictures/{$this->profile_picture}") : null,
             'company' => $this->mentor?->company,
             'expertise' => $this->mentor?->expertise,
